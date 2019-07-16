06da996a489b57a24c147046f65f188797cdfa6c
bno1 <ealex95@gmail.com>
2019-07-16 03:26:47 +0300

    added some fibonacci utils and more tests

--- src/Common.hs
+++ src/Common.hs
@@ -1,7 +1,30 @@
 module Common where
 
+goldenRatio :: Double
+goldenRatio = 1.6180339887498948482045868343656381177203091798057628621354486
+
+sqrt5 :: Double
+sqrt5 = sqrt 5
+
 nSum :: (Integral a) => a -> a
-nSum n = n * (n + 1) `quot` 2
+nSum n
+  | n > 0 = n * (n + 1) `quot` 2
+  | otherwise = 0
 
 sumOfMults :: (Integral a) => a -> a -> a
-sumOfMults limit n = n * nSum ((limit - 1) `quot` n)
+sumOfMults limit n
+  | limit <= 0 = 0
+  | otherwise = n * nSum ((limit - 1) `quot` n)
+
+fastFib :: (Integral a) => a -> a
+fastFib n = round ((goldenRatio ^ n) / sqrt5)
+
+approxFibLt :: (Integral a) => a -> a
+approxFibLt n = floor $ (lns5 + lnn) / lngr
+  where
+    lns5 = log sqrt5
+    lngr = log goldenRatio
+    lnn = log $ fromIntegral n
+
+fastFibSum :: (Integral a) => a -> a
+fastFibSum n = fastFib (n + 2) - 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,20 @@
 import Test.Hspec
+import Test.QuickCheck
 import Lib
+import Common
 
 main :: IO ()
 main = hspec $ do
+  it "Sum N" $ property $ \n -> nSum (n :: Int) == sum [1..n]
+  it "Sum Mult N" $ property $
+    \n lim -> (n > 0) ==>
+      sumOfMults lim (n :: Word) == sum (takeWhile (<lim) [n,2*n..])
+  it "FastFib" $ map fastFib [0, 1, 2] `shouldBe` [0, 1, 1]
+  it "FastFib 2" $ property $
+    \n -> (n > 1 && n < 76) ==>
+      fastFib (n :: Word) == fastFib (n - 1) + fastFib (n - 2)
+  it "FastFibSum" $ property $
+    \n -> (n > 1 && n < 74) ==>
+      fastFibSum (n :: Word) == sum (map fastFib [1..n])
   it "Fails gracefully" $ runProblem 999999 `shouldBe` "Unknown problem"
   it "Solves #1" $ runProblem 1 `shouldBe` "233168"
